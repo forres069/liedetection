27e0afffb172a0e1c97ea2fafa546f4ec42022b3
MikeLugovik <fores069@icloud.com>
2024-08-18 13:46:22 +0600

    Added vercel.json

--- liedetection/liedetection/settings.py
+++ liedetection/liedetection/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-rvoxrotm(@*2leduul+l7_zg_$i3z^bcv!sbkj)g0q__r*mka_"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', ]
+
 
 
 # Application definition
